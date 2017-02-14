4a60c849cafdf8c7a414edd9126dc5c273b8a30a
MrWigglez <rdsx1112@gmail.com>
2017-02-13 23:45:51 -0500

    Created Import Teams Method

--- circleOfLoss/Main.java
+++ circleOfLoss/Main.java
@@ -9,22 +9,38 @@ public class Main {
 	public static void main(String args[])
 	{
 		System.out.println("Hello World");
+		importTeams();
 	}
 	
 	
-	
-	public static ArrayList<ArrayList<Integer>> importTeams()
+	//ArrayList<ArrayList<Integer>>
+	public static void	importTeams()
 	{	
 		try{
 			String directory = System.getProperty("user.dir");
 			File textFile = new File(directory + "/" + "2016_GamesV2.txt");
 			
-			ArrayList<ArrayList<Integer>> teamRecords =  new ArrayList<ArrayList<Integer>>();
+			ArrayList<Node> teamRecords =  new ArrayList<Node>();
 			Scanner sc = new Scanner(textFile);
-		} catch (Exception e){
 			
+			int i = 1;
+			
+				String readLine = sc.nextLine();
+				String[] stringArray = readLine.split(",");
+				
+				Integer wTeamNumber = Integer.parseInt(stringArray[2]);
+				Integer lTeamNumber = Integer.parseInt(stringArray[5]);
+				
+				Node winningTeam = new Node(wTeamNumber);
+				winningTeam.addToBeaten(lTeamNumber);
+				
+				teamRecords.add(winningTeam);
+			
+			
+		} catch (Exception e){
+			e.printStackTrace();
 		}
 			
-		return teamRecords;
+		//return teamRecords;
 	}
 }
