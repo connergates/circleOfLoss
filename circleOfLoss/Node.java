cb1b358f3bbd3d8b9f18023d09e25deb45856ae9
connergates <trollinmon.real@gmail.com>
2017-02-13 23:06:38 -0500

    Added  Node Class
    Node class represents the Vertexes along with the verteices each edge
    goes to from it.

---
+++ circleOfLoss/Node.java
@@ -0,0 +1,37 @@
+package circleOfLoss;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Node implements Comparator<Node> {
+	private int teamNumber;
+	private ArrayList<Integer> beaten; 
+
+	public int getTeamNumber() {
+		return teamNumber;
+	}
+
+	public void setTeamNumber(int teamNumber) {
+		this.teamNumber = teamNumber;
+	}
+
+	public void addToBeaten(Integer loser)
+	{
+		beaten.add(loser);
+	}
+
+	@Override
+	public int compare(Node o1, Node o2) {
+		if(o1.getTeamNumber() > o2.getTeamNumber())
+		{
+			return 1;
+		}
+		else if(o1.getTeamNumber() < o2.getTeamNumber())
+		{
+			return -1;
+		}
+		return 0;
+		
+	}
+
+}
