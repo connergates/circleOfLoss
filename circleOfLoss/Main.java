ee3548b385eccd5b4c4ec93b9223edbabb98b854
MrWigglez <rdsx1112@gmail.com>
2017-02-14 22:44:26 -0500

    Created loop to read .txt
    Might need to change, not sure how to add an element to a specific
    position in an arraylist

--- circleOfLoss/Main.java
+++ circleOfLoss/Main.java
@@ -20,22 +20,39 @@ public class Main {
 			String directory = System.getProperty("user.dir");
 			File textFile = new File(directory + "/" + "2016_GamesV2.txt");
 			
-			ArrayList<Node> teamRecords =  new ArrayList<Node>();
+			//NEED TO CHECK FOR THIS NUMBER 
+			int numTeams = 351;
+			
+			ArrayList<Node> teamRecords =  new ArrayList<Node>(numTeams);
+
 			Scanner sc = new Scanner(textFile);
 			
+			//NEED WAY TO CHECK NUM OF LINES IN TXT FILE
+			int numLines = 5516;
 			int i = 1;
+			while (i < numLines) { 
 			
 				String readLine = sc.nextLine();
 				String[] stringArray = readLine.split(",");
 				
-				Integer wTeamNumber = Integer.parseInt(stringArray[2]);
-				Integer lTeamNumber = Integer.parseInt(stringArray[5]);
-				
-				Node winningTeam = new Node(wTeamNumber);
-				winningTeam.addToBeaten(lTeamNumber);
-				
-				teamRecords.add(winningTeam);
+				Integer wTeamNumber = Integer.parseInt(stringArray[2].trim()) - 1;
+				Integer lTeamNumber = Integer.parseInt(stringArray[5].trim()) - 1;
 			
+				if (teamRecords.get(wTeamNumber) == null) {
+					Node winningTeam = new Node(wTeamNumber);
+					winningTeam.addToBeaten(lTeamNumber);
+					
+					teamRecords.add(wTeamNumber, winningTeam);
+				} else {
+					//NEED TO ADD WAY TO CHECK IF TEAM HAS ALREADY BEEN BEATEN
+					teamRecords.get(wTeamNumber).addToBeaten(lTeamNumber);	
+				}
+				
+				i++;
+				
+			}
+			
+			System.out.println("Nodes Created");
 			
 		} catch (Exception e){
 			e.printStackTrace();
